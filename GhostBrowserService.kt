c08129444b1e87800645bac45901e22bee57f4e6
adiweb12 <vsadith051@gmail.com>
2026-02-22 14:15:43 +0530

    Update GhostBrowserService.kt

--- GhostBrowserService.kt
+++ GhostBrowserService.kt
@@ -3,21 +3,12 @@ package com.example.ghostbrowser
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.webkit.CookieManager
-import android.webkit.WebStorage
 
 class GhostBrowserService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         PrivacyManager.nukeSession()
         return START_NOT_STICKY
     }
-    override fun onBind(intent: Intent?): IBinder? = null
-}
 
-object PrivacyManager {
-    fun nukeSession() {
-        CookieManager.getInstance().removeAllCookies(null)
-        CookieManager.getInstance().flush()
-        WebStorage.getInstance().deleteAllData()
-    }
+    override fun onBind(intent: Intent?): IBinder? = null
 }
