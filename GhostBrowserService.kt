a468b271c07f7bb7f807f4bb85a23ad03afd8ef0
adiweb12 <vsadith051@gmail.com>
2026-02-22 11:32:17 +0530

    Update GhostBrowserService.kt

--- GhostBrowserService.kt
+++ GhostBrowserService.kt
@@ -5,22 +5,18 @@ import android.content.Intent
 import android.os.IBinder
 import android.webkit.CookieManager
 import android.webkit.WebStorage
-import android.webkit.WebView
 
 class GhostBrowserService : Service() {
-    
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // High-speed cleanup logic
-        clearAppData()
-        return START_NOT_STICKY
-    }
 
-    private fun clearAppData() {
-        CookieManager.getInstance().removeAllCookies(null)
-        CookieManager.getInstance().flush()
-        WebStorage.getInstance().deleteAllData()
-        // The service can now stop itself after cleaning
-        stopSelf()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // High Privacy Purge
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.removeAllCookies {
+            cookieManager.flush()
+            WebStorage.getInstance().deleteAllData()
+        }
+        
+        return START_NOT_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
