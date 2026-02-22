006c70e39ac548dd35c7fe7b141f1ecb3c398d21
adiweb12 <vsadith051@gmail.com>
2026-02-22 12:04:42 +0530

    Update GhostVpnService.kt

--- GhostVpnService.kt
+++ GhostVpnService.kt
@@ -8,23 +8,23 @@ class GhostVpnService : VpnService() {
     private var vpnInterface: ParcelFileDescriptor? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // Build the VPN Interface
         val builder = Builder()
         builder.setSession("GhostVPN")
-            .addAddress("10.0.0.1", 24) // Local Virtual IP
-            .addDnsServer("8.8.8.8")    // Secure Google DNS
-            .addRoute("0.0.0.0", 0)     // Route ALL traffic through here
+            .addAddress("10.0.0.1", 24)
+            .addDnsServer("8.8.8.8")
+            .addRoute("0.0.0.0", 0)
 
         vpnInterface = builder.establish()
-        
-        // At this point, packets are hitting your 'vpnInterface'.
-        // For professional 'packeting', you would read from this file descriptor.
-        
         return START_STICKY
     }
 
     override fun onDestroy() {
-        vpnInterface?.close()
+        try {
+            vpnInterface?.close()
+            vpnInterface = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
         super.onDestroy()
     }
 }
